f6d7275c618ca0917b74c8338d1627b6b5805be0
unknown <mtsheikh@ualberta.ca>
2018-08-22 09:18:18 -0600

    Section 4

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-using System.Data.Entity;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,6 +20,61 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        // GET: Customers/Edit
+        public ActionResult Edit(int id)
+        {
+            // Retrieve the Customer based on the If from the Database
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            // If customer doesn't exist take to page 404
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        // POST: Customers/Create
+        // This Action creates a user and adds it to DB, it works with the Create form in CustomerForm.cshtml page
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        // GET: Customers/New
+        // This Action grabs the membership lists from DB and displays it to CustomerFormViewModel
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
@@ -31,6 +84,7 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        // GET: Customers/Details/[id]
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
