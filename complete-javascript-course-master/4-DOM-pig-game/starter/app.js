1c01690e95e38d0b976a0d3f59682c58beffea02
unknown <mtsheikh@ualberta.ca>
2018-12-13 12:32:06 -0700

    Added an extra dice

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -18,28 +18,37 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     // 1. Random number
 
     var dice = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display the result
+    // 2. Display the result for dice 1
     var diceDOM = document.querySelector(".dice");
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + ".png";
 
+    // 2b. Display the result for dice 2
+    var dice2DOM = document.querySelector(".dice2");
+    dice2DOM.style.display = "block";
+    dice2DOM.src = "dice-" + dice2 + ".png";
+
     // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1 && (dice !== 6 && lastDiceRoll !== 6)) {
-      //Add score
-      roundScore += dice;
-      document.querySelector(
-        "#current-" + activePlayer
-      ).textContent = roundScore;
-    } else if (dice === 6 && lastDiceRoll === 6) {
+    if (dice === 6 && dice2 === 6) {
       roundScore = 0;
       document.querySelector("#score-" + activePlayer).textContent = 0;
       nextPlayer();
+    } else if (dice !== 1 && dice2 !== 1) {
+      // && (dice !== 6 && dice2 !== 6)
+      //Add score
+      roundScore += dice;
+      roundScore += dice2;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
     } else {
       //Next player
       nextPlayer();
     }
-    lastDiceRoll = dice;
+
+    console.log(dice, dice2, activePlayer);
   }
 });
 
@@ -89,6 +98,7 @@ function nextPlayer() {
 
   //Reset the Dice
   document.querySelector(".dice").style.display = "none";
+  document.querySelector(".dice2").style.display = "none";
 }
 
 function init() {
@@ -99,6 +109,7 @@ function init() {
   scoreRequired = 55;
 
   document.querySelector(".dice").style.display = "none";
+  document.querySelector(".dice2").style.display = "none";
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
   document.getElementById("current-0").textContent = "0";
